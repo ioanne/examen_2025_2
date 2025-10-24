6a93cd1920b9d7fa0b6627e851d4e114feab1751
nadiradad <nadiradad@gmail.com>
2025-10-24 19:57:29 -0300

    creando el model y el admin de profesor

--- gestion_academica/apps/profesor/models.py
+++ gestion_academica/apps/profesor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
