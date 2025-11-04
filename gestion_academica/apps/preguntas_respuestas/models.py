2250879e3c277ea8d1a09551e9c7e51b6133d52e
Ludmila Brizuela <brizuelal127@gmail.com>
2025-11-04 19:23:56 -0300

    G4-003: Creación de modelos Pregunta y Respuesta

--- gestion_academica/apps/preguntas_respuestas/models.py
+++ gestion_academica/apps/preguntas_respuestas/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Pregunta(models.Model):
+    examen = models.ForeignKey('examenes.Examen', on_delete=models.CASCADE, related_name='preguntas')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.titulo
+
+class Respuesta(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE, related_name='respuestas')
+    texto_respuesta = models.TextField(blank=True)
+    es_correcta = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"Respuesta a: {self.pregunta.titulo}"
