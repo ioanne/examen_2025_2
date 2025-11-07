a364c0ff5c921c9aede8190da1eab7b26f809d35
teosordo <teosordo98@gmail.com>
2025-11-06 23:05:00 -0300

    definicion foreignkeys app cursada

--- gestion_academica/apps/cursada/models.py
+++ gestion_academica/apps/cursada/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from apps.profesor.models import Profesor
+from apps.asignatura.models import Asignatura
 
 class Cursada(models.Model):
     ESTADOS = [
@@ -10,6 +12,9 @@ class Cursada(models.Model):
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
     estado = models.CharField(max_length=20, choices=ESTADOS, default='Activa')
+
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, related_name='cursada')
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE, related_name='cursada')
 
     def __str__(self):
         return f"{self.nombre} ({self.estado})"
