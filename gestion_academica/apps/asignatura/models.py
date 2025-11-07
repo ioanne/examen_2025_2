68962221fff44b6ada58f1225aa67b586da7dbc1
teosordo <teosordo98@gmail.com>
2025-11-06 23:06:36 -0300

    creacion app asignatura

---
+++ gestion_academica/apps/asignatura/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Asignatura(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=20, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nombre}"
